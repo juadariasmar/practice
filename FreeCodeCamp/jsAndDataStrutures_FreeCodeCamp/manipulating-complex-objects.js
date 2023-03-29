66f5aa183a29a8f3bab4c4b8f945c9f42d2f5b1a
Juan David Arias <45865805+juadariasmar@users.noreply.github.com>
2023-03-28 23:36:11 -0500

    add file practice freecodecamp course

---
+++ FreeCodeCamp/jsAndDataStrutures_FreeCodeCamp/manipulating-complex-objects.js
@@ -0,0 +1,19 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    artist: "ACDC",
+    title: "Back in black",
+    release_year: 1975,
+    formats: ["cd", "87", "43"]
+  }
+];
