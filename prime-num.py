6e25de3bd0f8d88344259f2efc44e824b3fccb78
juadariasmar <juadariasmar@gmail.com>
2023-04-10 01:40:00 -0500

    creation of script that validates prime numbers up to 250 and saves the result in a txt file: results.txt

---
+++ prime-num.py
@@ -0,0 +1,13 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+with open("results.txt", "w") as file:
+    for num in range(1, 251):
+        if is_prime(num):
+            file.write(str(num) + "\n")
+            print(num)
